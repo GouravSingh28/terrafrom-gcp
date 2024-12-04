35751b452564adf5b330db54e5af51e405142b5f
GouravSingh28 <67264031+GouravSingh28@users.noreply.github.com>
2024-12-04 13:13:18 +0530

    default values updated

--- terrafrom-vm-module/variables.tf
+++ terrafrom-vm-module/variables.tf
@@ -1,11 +1,13 @@
 variable "project_id" {
   description = "GCP Project ID"
   type        = string
+  default     = "terraform-learning-442205"
 }
 
 variable "region" {
   description = "GCP Region"
   type        = string
+  default     = "us-central1"
 }
 
 variable "zone" {
@@ -17,21 +19,25 @@ variable "zone" {
 variable "vm_name" {
   description = "Name of the VM"
   type        = string
+  default     = "test-vm1-us"
 }
 
 variable "machine_type" {
   description = "Machine type (e.g., e2-medium, n1-standard-1)"
   type        = string
+  default     = "e2-micro" 
 }
 
 variable "disk_image" {
   description = "Source image for the boot disk"
   type        = string
+  default     = "debian-cloud/debian-11"
 }
 
 variable "network" {
   description = "VPC network name"
   type        = string
+  default     = "default"
 }
 
 variable "metadata" {
@@ -49,6 +55,7 @@ variable "tags" {
 variable "service_account_email" {
   description = "Service account email"
   type        = string
+  default     = "gouravsingh335@gmail.com"
 }
 
 variable "service_account_scopes" {
