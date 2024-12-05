a7b9f25ceabe7ad46c704c0c3f647fec1b402666
GouravSingh28 <67264031+GouravSingh28@users.noreply.github.com>
2024-12-05 12:37:04 +0530

    Update main.tf

--- terrafrom-vm-module/main.tf
+++ terrafrom-vm-module/main.tf
@@ -1,16 +1,17 @@
 # Configure the Google Cloud provider
 variable "google_credentials" {
   description = "Path to the GCP service account JSON file"
- }
+}
+
 provider "google" {
   credentials = base64decode(var.google_credentials) # Replace with your JSON key path
   project     = "terraform-learning-442205" # Replace with your GCP Project ID
   region      = "us-central1"
- }
+}
 
 data "google_compute_zones" "available" {
   region = var.region
- }
+}
 
 resource "google_compute_instance" "vm_instance" {
   name         = var.vm_name
@@ -30,19 +31,17 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 
-resource "google_compute_instance" "vm_instance" {
   metadata = {
     enable-oslogin = "TRUE"
+  }
+
+  service_account {
+    email  = var.service_account_email
+    scopes = var.service_account_scopes
   }
 }
 
 resource "google_os_login_ssh_public_key" "default" {
   user = "gouravsingh335@gmail.com" # Replace with your GCP email
-  key  = file("./access-key.pub")
+  key  = file("./access-key.pub")   # Path to your public key file
 }
-
-  service_account {
-    email  = var.service_account_email
-    scopes = var.service_account_scopes
-  }  
-  }
